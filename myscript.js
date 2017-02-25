13baaad83dc9b57982dd2972bfe7872105c498c7
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2017-02-25 16:02:41 +0600

    extension functionality complete

--- myscript.js
+++ myscript.js
@@ -5,24 +5,86 @@
 // get current tab url
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var currentUrl = tabs[0].url;
-    document.getElementById('url').innerHTML = currentUrl;
+    //  chrome.tabs.getSelected(null, function(tab) {
+    // var currentUrl = document.getElementById('url').innerHTML = tab.url;
+     // console.log(tab.url);
+    // });
+    // document.getElementById('url').innerHTML = currentUrl;
     // alert(url);
     var apiUrlToHit = "https://sustcse12.xyz/api/trs/show";
     var linkToBeSent = encodeURIComponent(currentUrl);
-    var finalURL = apiUrlToHit+linkToBeSent;
+    var finalURL = apiUrlToHit+'?link='+linkToBeSent;
     console.log(finalURL);
     $.ajax({
-	    url:  finalURL,
-	    dataType: "json",
-	    type : "GET",
+        type : "POST",
+	    url:  apiUrlToHit,
+        data: { link: linkToBeSent },
+	    dataType: "json", 
 	    success : function(response) {
 	      console.log(response);
 	      document.getElementById('score').innerHTML = response.score;
 	    },
 	    error : function(response) {
 	    	console.log(response);
+            console.log(response.responseText);
 	    }
   	});
+
+    $("#submit").click(function(e){
+        // e.preventDefault();
+
+        var formData = $('form#formOfRating').serialize();
+        var gg = $('form#formOfRating').find('input[name="rating"]:checked').val();
+        var apiUrlToHit = "https://sustcse12.xyz/api/trs/post";
+ 
+        // console.log(formData);
+        var dataToBeSent = {
+            'rating': gg,
+            'link' : linkToBeSent
+        }
+        // console.log(formData);
+        // console.log('djkf');
+        // // formData['url'] = linkToBeSent;
+
+        // console.log(gg);
+
+        // console.log(dataToBeSent);
+        // $.extend(true, formData, newdata);
+
+        // alert(linkToBeSent);
+        // alert(currentUrl);
+
+        // console.log(newdata);
+        // alert(dataToBeSent);
+        // alert(apiUrlToHit);
+        // $('#formDiv').html("<p>Hi</p>");
+        $.ajax({
+            type: "POST",
+            url: apiUrlToHit,
+            data: dataToBeSent,
+            dataType: 'json',
+          
+            success: function(response){
+                console.log('ok success');
+                console.log(response);
+                // if(response.status_code == '201') {
+                    var message = response.success;
+                    console.log(message);
+                    var message = 'Thank your for your rating';
+                    
+                    // $('#formDiv').html('');
+                    $('#formDiv').html(message);
+                // }
+            },
+            error: function(response){
+                console.log(response);
+                console.log('Not ok, Failed');
+                // var message = 'Something Went Wrong';
+                var message = "";
+                // console.log(response);      
+            }
+        }); // end of ajax
+    }); 
 
 });
 
@@ -41,37 +103,37 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
  * @apiSuccess {Float}  score Score of the Requested URL.
  */
 
-				$("#submit").click(function(e){
-                    e.preventDefault();
+				// $("#submit").click(function(e){
+    //                 e.preventDefault();
 
-                    var $form = $(this);
-                    // var $target = $($form.attr('data-target'));
-                    var apiUrlToHit = "https://sustcse12.xyz/api/trs";
-                    var dataToBeSent = $('form#formOfRating').serialize();
-                    console.log(dataToBeSent);
-                    $.ajax({
-                        type: "POST",
-                        url: apiUrlToHit,
-                        data: dataToBeSent,
-                        dataType: 'json',
-                        function(data) {
-                            console.log(data);
-                        },
-                        success: function(response){
-                            console.log('ok success');
-                            console.log(response);
-                            // if(response.status_code == '201') {
-                             	// var message = 'Successfull';
-                                var message = response.message;
-                                $('#success').html('');
-                                $('#success').html(message);
-                            // }
-                        },
-                        error: function(response){
-                            console.log('Not ok, Failed');
-                            // var message = 'Something Went Wrong';
-                            var message = "";
-                            console.log(response);      
-                        }
-                    }); // end of ajax
-                }); // end of submit button click 
+    //                 var $form = $(this);
+    //                 // var $target = $($form.attr('data-target'));
+    //                 var apiUrlToHit = "https://sustcse12.xyz/api/trs";
+    //                 var dataToBeSent = $('form#formOfRating').serialize();
+    //                 console.log(dataToBeSent);
+    //                 $.ajax({
+    //                     type: "POST",
+    //                     url: apiUrlToHit,
+    //                     data: dataToBeSent,
+    //                     dataType: 'json',
+    //                     function(data) {
+    //                         console.log(data);
+    //                     },
+    //                     success: function(response){
+    //                         console.log('ok success');
+    //                         console.log(response);
+    //                         // if(response.status_code == '201') {
+    //                          	// var message = 'Successfull';
+    //                             var message = response.message;
+    //                             $('#success').html('');
+    //                             $('#success').html(message);
+    //                         // }
+    //                     },
+    //                     error: function(response){
+    //                         console.log('Not ok, Failed');
+    //                         // var message = 'Something Went Wrong';
+    //                         var message = "";
+    //                         console.log(response);      
+    //                     }
+    //                 }); // end of ajax
+    //             }); // end of submit button click 
